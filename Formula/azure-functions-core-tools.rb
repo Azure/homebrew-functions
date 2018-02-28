153db42d63f67db3406cc77508f263515d00c03f
watashiSHUN <mike53669@gmail.com>
2018-02-27 17:26:43 -0800

    add formula for azure-functions-core-tools

---
+++ Formula/azure-functions-core-tools.rb
@@ -0,0 +1,22 @@
+class AzureFunctionsCoreTools < Formula
+  desc "Azure Function Cli 2.0"
+  homepage "https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#run-azure-functions-core-tools"
+  url "https://functionscdn.azureedge.net/public/2.0.1-beta.22/osx/azure.functions.cli.osx-x64.2.0.1-beta.22.zip"
+  version "2.0.1-beta.22"
+  sha256 "d99f03b54c399479246d6c395dd1fa93092769796e7a5bbd5c6b5da1c07fd766"
+  head "https://github.com/Azure/azure-functions-core-tools"
+
+  bottle :unneeded
+
+  def install
+    prefix.install Dir["*"]
+    chmod 0555, prefix/"func"
+    bin.install_symlink prefix/"func"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/func")
+    system bin/"func", "new", "-l", "C#", "-t", "HttpTrigger", "-n", "confusedDevTest"
+    assert_predicate testpath/"confusedDevTest/function.json", :exist?
+  end
+end
