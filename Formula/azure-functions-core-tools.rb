de39b134b5d9418a87657cf5a098ce6b089152cc
soninaren <soninaren@users.noreply.github.com>
2019-04-29 17:13:26 -0700

    updating cli version to 2.6.1080

--- Formula/azure-functions-core-tools.rb
+++ Formula/azure-functions-core-tools.rb
@@ -1,10 +1,10 @@
 class AzureFunctionsCoreTools < Formula
   desc "Azure Function Cli 2.0"
   homepage "https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#run-azure-functions-core-tools"
-  url "https://functionscdn.azureedge.net/public/2.6.1048/Azure.Functions.Cli.osx-x64.2.6.1048.zip"
-  version "2.6.1048"
+  url "https://functionscdn.azureedge.net/public/2.6.1080/Azure.Functions.Cli.osx-x64.2.6.1080.zip"
+  version "2.6.1080"
   # make sure sha256 is lowercase.
-  sha256 "53a996560371d340640cf9d52e564d90e13143a5f2415295ab40ab8d41e088e1"
+  sha256 "0d331bcdd7e9d8ee2026d10db93b8b71239063a1db9a65666ca06510e51c9489"
   head "https://github.com/Azure/azure-functions-core-tools"
 
   bottle :unneeded
