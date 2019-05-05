6c3441400f42daba0cfc974aa27f6fb92d470a31
Ahmed ElSayed <ahmels@microsoft.com>
2019-05-05 12:04:34 -0700

    Update to 2.7.1149

--- Formula/azure-functions-core-tools.rb
+++ Formula/azure-functions-core-tools.rb
@@ -1,10 +1,10 @@
 class AzureFunctionsCoreTools < Formula
   desc "Azure Function Cli 2.0"
   homepage "https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#run-azure-functions-core-tools"
-  url "https://functionscdn.azureedge.net/public/2.6.1080/Azure.Functions.Cli.osx-x64.2.6.1080.zip"
-  version "2.6.1080"
+  url "https://functionscdn.azureedge.net/public/2.7.1149/Azure.Functions.Cli.osx-x64.2.7.1149.zip"
+  version "2.7.1149"
   # make sure sha256 is lowercase.
-  sha256 "0d331bcdd7e9d8ee2026d10db93b8b71239063a1db9a65666ca06510e51c9489"
+  sha256 "69fb1e542f7e488d371b58876b46d4fccd2e48b946964177cc71d1a80c1e7148"
   head "https://github.com/Azure/azure-functions-core-tools"
 
   bottle :unneeded
