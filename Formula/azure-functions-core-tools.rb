ae50fbfcaac2a352d425910ab8ad2cfa9b7d781e
Ankit Kumar <ankikuma@microsoft.com>
2019-08-15 17:21:16 -0700

    Release anotha core tools

--- Formula/azure-functions-core-tools.rb
+++ Formula/azure-functions-core-tools.rb
@@ -1,10 +1,10 @@
 class AzureFunctionsCoreTools < Formula
   desc "Azure Function Cli 2.0"
   homepage "https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#run-azure-functions-core-tools"
-  url "https://functionscdn.azureedge.net/public/2.7.1524/Azure.Functions.Cli.osx-x64.2.7.1524.zip"
+  url "https://functionscdn.azureedge.net/public/2.7.1558/Azure.Functions.Cli.osx-x64.2.7.1558.zip"
   version "2.7.1505"
   # make sure sha256 is lowercase.
-  sha256 "d907678486fed3230883baa89fe45d8bff003bca9ad494da26732e3dca79fea7"
+  sha256 "00a707f42789aa80453af83bd22e1bcdb0307d6d91195c25129bbf0761059cf4"
   head "https://github.com/Azure/azure-functions-core-tools"
 
   bottle :unneeded
