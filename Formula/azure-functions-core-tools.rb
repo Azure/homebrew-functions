7c32e82592e99b68b53d4c23b8393de563d80fe2
Ankit Kumar <ankikuma@microsoft.com>
2019-09-18 10:37:57 -0700

    Update FUNCTIONS_CLI to FUNCTIONS_CORE_TOOLS

--- Formula/azure-functions-core-tools.rb
+++ Formula/azure-functions-core-tools.rb
@@ -11,7 +11,7 @@ class AzureFunctionsCoreTools < Formula
 
   @@telemetry = "\n Telemetry \n --------- \n The Azure Functions Core tools collect usage data in order to help us improve your experience." \
   + "\n The data is anonymous and doesn\'t include any user specific or personal information. The data is collected by Microsoft." \
-  + "\n \n You can opt-out of telemetry by setting the FUNCTIONS_CLI_TELEMETRY_OPTOUT environment variable to \'1\' or \'true\' using your favorite shell.\n"
+  + "\n \n You can opt-out of telemetry by setting the FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT environment variable to \'1\' or \'true\' using your favorite shell.\n"
 
   def install
     prefix.install Dir["*"]
