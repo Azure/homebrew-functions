e4e2de7f8a2b809f796eeba93bfb50657b5da194
p-r-a-s-h-a-n-t <56632757+p-r-a-s-h-a-n-t@users.noreply.github.com>
2019-10-16 11:22:25 +0100

    Resolve install/upgrade issue
    The current formula downloads incorrect version causing mismatch in Checksum.

--- Formula/azure-functions-core-tools.rb
+++ Formula/azure-functions-core-tools.rb
@@ -1,7 +1,7 @@
 class AzureFunctionsCoreTools < Formula
   desc "Azure Function Cli 2.0"
   homepage "https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#run-azure-functions-core-tools"
-  url "https://functionscdn.azureedge.net/public/2.7.1704/Azure.Functions.Cli.osx-x64.2.7.1704.zip"
+  url "https://functionscdn.azureedge.net/public/2.7.1724/Azure.Functions.Cli.osx-x64.2.7.1724.zip"
   version "2.7.1724"
   # make sure sha256 is lowercase.
   sha256 "95cb70382edc50e2264de0b41839476618c946d18fd8067ed0933f685e50f89e"
