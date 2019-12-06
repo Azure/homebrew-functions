f83bfee609cab305ad274a501ff865485e95409f
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2019-12-06 10:13:53 -0800

    Re-adding preview formula

---
+++ Formula/azure-functions-core-tools-v3-preview.rb
@@ -0,0 +1,32 @@
+class AzureFunctionsCoreToolsV3Preview < Formula
+  desc "Azure Function Cli 3.0"
+  homepage "https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#run-azure-functions-core-tools"
+  url "https://functionscdn.azureedge.net/public/3.0.1958/Azure.Functions.Cli.osx-x64.3.0.1958.zip"
+  version "3.0.1958"
+  # make sure sha256 is lowercase.
+  sha256 "828f35352c873872fc6f640a45939f6f7365bd0821ad29d4b7a386fdba2cc139"
+  head "https://github.com/Azure/azure-functions-core-tools"
+
+  bottle :unneeded
+
+  @@telemetry = "\n Telemetry \n --------- \n The Azure Functions Core tools collect usage data in order to help us improve your experience." \
+  + "\n The data is anonymous and doesn\'t include any user specific or personal information. The data is collected by Microsoft." \
+  + "\n \n You can opt-out of telemetry by setting the FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT environment variable to \'1\' or \'true\' using your favorite shell.\n"
+
+  def install
+    prefix.install Dir["*"]
+    chmod 0555, prefix/"func"
+    bin.install_symlink prefix/"func"
+    begin
+      FileUtils.touch(prefix/"telemetryDefaultOn.sentinel")
+      print @@telemetry
+    rescue Exception
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/func")
+    system bin/"func", "new", "-l", "C#", "-t", "HttpTrigger", "-n", "confusedDevTest"
+    assert_predicate testpath/"confusedDevTest/function.json", :exist?
+  end
+end
