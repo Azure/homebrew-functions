d72da45877da71d2520ddc15c1717c8b555a176c
Azure Functions Release <57925570+azfuncgh@users.noreply.github.com>
2024-05-16 16:20:39 -0700

    Updated version to 4.0.5801 (#153)
    Co-authored-by: azfuncgh <azfuncgh@github.com>

--- Formula/azure-functions-core-tools@4.rb
+++ Formula/azure-functions-core-tools@4.rb
@@ -1,14 +1,14 @@
 class AzureFunctionsCoreToolsAT4 < Formula
-  funcVersion = "4.0.5700"
+  funcVersion = "4.0.5801"
   if OS.linux?
     funcArch = "linux-x64"
-    funcSha = "83bb2f9c5039676d8295fe366019f122d3c34bd2d610c27ad906d152be3a2022"
+    funcSha = "96a3d52fe82728875b6052551de19a733b7b234aa7333261336cd74747ab368b"
   elsif Hardware::CPU.arm?
     funcArch = "osx-arm64"
-    funcSha = "79f90794522e4075092d3b95c54d956059aba159a168673c7fb367a06c9aa963"
+    funcSha = "57c7e4d9bfc58d647a77ca9fc48326af7d703051d1c540b3363a9bc5f3217fb7"
   else
     funcArch = "osx-x64"
-    funcSha = "31a0c798bb66ae80f07e26c179c01a6e501fd18388537fcbea95b8d53817b60e"
+    funcSha = "7e6ae1d46c868e46fd56156cb452390bb7e208a788f95aa0c3409cefe4963214"
   end
 
   desc "Azure Functions Core Tools 4.0"
